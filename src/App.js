bdfcd465ad9bd44091e41a4feba8ad82e503e2d8
Jaime Salazar <langsamlernen@gmail.com>
2022-07-27 01:12:34 -0500

    API query

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import AsideWeather from './components/AsideWeather';
 import SearchBox from './components/SerachBox';
 
-const dirApi = `https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=206a73512f32dfa611eccd13c89d9fef`
-
 function App() {
     return (
         <div className="App">
--- src/components/SerachBox.js
+++ src/components/SerachBox.js
@@ -1,17 +1,36 @@
+import { useEffect, useState } from "react";
+
 const SearchBox = () => {
+    // const dirApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=206a73512f32dfa611eccd13c89d9fef`
+    const [location, setLocation] = useState(null);
+    const [locationData, setLocationData] = useState(null) ;
+
+    const handleClick = async(event) => {
+        event.preventDefault();
+        event.target.reset();
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=206a73512f32dfa611eccd13c89d9fef`);
+        const result = await response.json();
+        setLocationData(result);
+        console.log(result);
+    }
+
+    const handleInputChange = (event) => {
+        setLocation(event.target.value);
+    }
+
     function toggleMenu() {
         const btn = document.getElementById("search-box");
         btn.classList.toggle("hidden");
     }
-    
+
     return (
         <>
             <div className="control">
                 <button className="btn" id="btn-close" onClick={toggleMenu}><i className="fa-solid fa-xmark fa-lg"></i></button>
             </div>
-            <form className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Place" aria-label="Place" aria-describedby="button-addon2" />
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2">Buscar</button>
+            <form onSubmit={handleClick} className="input-group mb-3">
+                <input type="text" name="place" onChange={handleInputChange} className="form-control" placeholder="Place" aria-label="Place" aria-describedby="button-addon2" />
+                <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Buscar</button>
             </form>
         </>
     );
