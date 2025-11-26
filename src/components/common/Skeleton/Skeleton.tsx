ac72ae40ac0bfbeb4a82a5c1ad3072a1fd68d1e3
JaimeSag <langsamlernen@gmail.com>
2025-11-26 11:15:02 -0600

    Add Skeleton component

---
+++ src/components/common/Skeleton/Skeleton.tsx
@@ -0,0 +1,28 @@
+import { CSSProperties } from "react";
+import styles from './Skeleton.module.css';
+
+type SkeletonProps = {
+  className?: string,
+  style?: CSSProperties,
+  width?: string | number,
+  height?: string | number
+};
+
+export function Skeleton({
+  className,
+  style,
+  width,
+  height
+}: SkeletonProps) {
+  return (
+    <div
+      className={`${styles.skeleton} ${className}`}
+      style={{
+        width,
+        height,
+        ...style
+      }}
+      aria-hidden="true"
+    />
+  );
+}
