19e40a05f972cedf52dfd6e7e51f2e7b455bb5d9
JaimeSag <langsamlernen@gmail.com>
2025-11-25 21:35:42 -0600

    Refactor ESLint configuration and add Stylelint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,5 +20,18 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/semi': "error",
+      '@stylistic/jsx-indent-props': ["error", 2],
+      '@stylistic/jsx-closing-bracket-location': ["error", "line-aligned"],
+      '@stylistic/jsx-closing-tag-location': ["error", "line-aligned"],
+      '@stylistic/jsx-max-props-per-line': "error",
+      '@stylistic/jsx-curly-newline': "error",
+      '@stylistic/arrow-parens': ["error", "as-needed"],
+    }
   },
-])
+]);
